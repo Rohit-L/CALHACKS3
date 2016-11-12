9a859dddcbd15fe2a4967941ece35eca911a1109
Rohit Lalchandani <rohitx15@berkeley.edu>
2016-11-12 11:17:24 -0800

    Using materialize and react now

--- app/assets/javascripts/react/react-libraries.js
+++
@@ -1,8 +0,0 @@
-// //React Library
-React = require('react');
-ReactDOM = require('react-dom');
-
-//Material Design Library
-material = require('material-ui');
-
-injectTapEventPlugin = require('react-tap-event-plugin'); injectTapEventPlugin();
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Photo.destroy_all
+Tag.destroy_all
+Caption.destroy_all
+User.destroy_all
